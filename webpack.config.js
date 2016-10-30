e65b43287f493cc390ec1d61428898d655eca4c8
Maxim Kerstens <maxim@commefort.com>
2016-10-30 19:52:30 +0100

    Added build command

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: ['./vue-echo.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'build.js',
+        library: ['VueEcho'],
+        libraryTarget: 'umd'
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules'),
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ]
+    },
+    devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = 'source-map'
+
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ])
+}
